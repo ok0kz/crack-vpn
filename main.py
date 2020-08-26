0cd3a535daffe703d1a3c309b91de5ad58605adc
m1ten <57693631+m1ten@users.noreply.github.com>
2020-08-25 20:31:22 -0400

    idk

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 
-executable_path = "./chromedriver"
-# os.environ["webdriver.chrome.driver"] = executable_path
+driver_path = "./chromedriver"
+os.environ["webdriver.chrome.driver"] = driver_path
 
-chrome_options = Options()
-chrome_options.add_extension('buster.crx')
+options = Options()
+options.add_extension('buster.crx')
 
-driver = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
 driver.get("https://google.com/recaptcha/api2/demo")
 frame = driver.find_element_by_xpath('//iframe[contains(@src, "recaptcha")]')
 driver.switch_to.frame(frame)
 driver.find_element_by_xpath("//*[@id='recaptcha-anchor']").click()
 time.sleep(3)
-driver.find_element_by_xpath("//iframe[@name='c-i50lxov4pf2d']").click()
+driver.find_element_by_title("Solve the challenge").click()
 
-time.sleep(10)
+time.sleep(100)
 
 
 driver.switch_to.default_content()
